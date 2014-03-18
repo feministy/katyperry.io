e16103c77af4d96f90d91ea94b46a388009f83f7
Liz Abinante <eabinante@gmail.com>
2014-03-18 16:43:41 -0600

    static controller tested wheeee

--- spec/controllers/static_spec.rb
+++ spec/controllers/static_spec.rb
@@ -2,8 +2,25 @@ require 'spec_helper'
 
 describe 'Static Controller' do
   context 'get /' do
-    it 'works' do
+    before do
       get '/'
+    end
+
+    it 'works' do
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'gets index.slim' do
+      expect(last_response.body).to include("The perkiest ipsum API ever.")
+    end
+  end
+
+  context 'get/css/*' do
+    before do
+      get '/css/application.css'
+    end
+
+    it 'works' do
       expect(last_response.status).to eq(200)
     end
   end
