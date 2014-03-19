51906520f9bf082f70c511d5427ebd81aa409da4
Liz Abinante <eabinante@gmail.com>
2014-03-18 20:55:00 -0600

    test for dictionary comparison

--- spec/controllers/api_v1_words_spec.rb
+++ spec/controllers/api_v1_words_spec.rb
@@ -2,10 +2,30 @@ require 'spec_helper'
 
 describe '/api/v/words.json' do
   before do
-    words = Helpers::DICTIONARY.map { |w| Word.create!(text: w) }
+    @dict = Helpers::DICTIONARY.map { |w| Word.create!(text: w) }
+    get 'api/v1/words.json&count=5'
+
+    result = JSON.parse(last_response.body)
+    @words = result["words"]
   end
 
   it 'works' do
-    get 'api/v1/words.json&count=5'
+    expect(last_response.status).to eq(200)
+  end
+
+  it 'returns an arary' do
+    expect(@words).to be_instance_of(Array)
+  end
+
+  it 'returns a uniq list' do
+    expect(@words.uniq.length).to eq(@words.length)
+  end
+
+  it 'returns 5 words' do
+    expect(@words.length).to eq(5)
+  end
+
+  it 'returns words from dictionary' do
+    expect(Helpers::DICTIONARY).to include(@words)
   end
 end
