b6cb43dcf1e789676625239a12e80257ae72ef67
Liz Abinante <eabinante@gmail.com>
2014-03-20 06:48:43 -0600

    data arrays moved to individual files

---
+++ db/data/words.rb
@@ -0,0 +1,16 @@
+WORDS = %w(
+    boys boy girl girls kissed kiss kissing one waking
+    woken wake woke vegas thinking you us thought think
+    mannequin hot cold warm cool freezing tepid afford
+    rich poor lost breathing breath breathe breathtaking
+    fingerprints pink purple teenage teenager dream dreaming
+    dreamer dreamy teen teens last first day night morning
+    evening afternoon peacock firework circle drain square
+    away extraterrestrial ET living live pearl hummingbird
+    heart heartbeat movies movie kitty purry roar legendary
+    lovers love loving loved legend legends birthday birth
+    walking air floating unconditionally dark horse light
+    pony cottoncandy fairy tale international smile ghost
+    moment momentary minute rainbow grace wish lover
+    graceless tacky little cute adorable turquoise clouds
+    )
